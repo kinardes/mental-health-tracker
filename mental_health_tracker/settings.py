fa1b01a9683bbcd58130e613efb7f8332631fa8f
kinardes <alicakinar@gmail.com>
2024-09-11 14:23:04 +0700

    Tutorial 2 Done

--- mental_health_tracker/settings.py
+++ mental_health_tracker/settings.py
@@ -123,4 +123,4 @@ STATIC_URL = 'static/'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-CSRF_TRUSTED_ORIGINS = ["http://localhost","http://127.0.0.1","http://http://alica-kinar-mentalhealthtracker.pbp.cs.ui.ac.id/", "https://<URL_PWS_KAMU>"]
+CSRF_TRUSTED_ORIGINS = ["http://localhost","http://127.0.0.1","http://alica-kinar-mentalhealthtracker.pbp.cs.ui.ac.id/", "https://alica-kinar-mentalhealthtracker.pbp.cs.ui.ac.id/"]
